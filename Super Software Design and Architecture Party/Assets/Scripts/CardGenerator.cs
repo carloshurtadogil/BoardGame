7190a72cf610e22fa798bb95e4b5b319d292ee6b
Carlos Hurtado <carlos.hurtado19@yahoo.com>
2018-10-08 18:41:19 -0700

    RNG Cards Fixed and Animated
    Cards will be randomly dealt and will appear in the world when called. Animation included. Value of the card can also be accessed.

--- Super Software Design and Architecture Party/Assets/Scripts/CardGenerator.cs
+++ Super Software Design and Architecture Party/Assets/Scripts/CardGenerator.cs
@@ -3,46 +3,67 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class CardGenerator : MonoBehaviour {
+public class CardGenerator : MonoBehaviour
+{
 
     public GameObject[] cards;//Array to hold all the cards that will determine the amount of spaces a player can move
+    private float count = 0.0f;
+    private int value = -1;
 
-	// Prepare cards before the game begins
-	void Start () {
+    // Prepare cards before the game begins
+    void Start()
+    {
         ScaleCards();
-        Draw();
+        StartCoroutine("Draw");
+        Debug.Log("Value: " + value);
     }
-	
-	// Update is called once per frame
-	void Update () {
-        Draw();
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if(count < 4.0f) {
+            count += Time.deltaTime;
+            //Debug.Log(count);
+        } else  {
+            //StartCoroutine ("Draw");
+        }
     }
 
     //Draw a card at random and instantiate it to the world. Includes animation
-    public void Draw() {
-        //@Casey: Use this method to random choose a value from 0 to cards.length exclusive (0,52), and call upon the index.
-        // No need to shuffle, just be random. This will save us time and memory.
-        // After you are able to generate random cards into the world, work on the animation. You will need to learn about Time.deltaTime and Euler Angles to do so using code.
-        //   unless of course you are able to find a better way.
-        // If you want to adjust where the cards appear in the world, just adjust the second parameter of the Instantiate method to do so; 
-        //    we do not yet need to attach the cards to any players during this sprint.
-        // If you have any questions, please do not hesitate in letting me know. THANK YOU!!!!
-        int index = 0;
-       index = Random.Range(0, cards.Length);
-        Instantiate(cards[index], new Vector3(0.0f, 22.0f, 0.0f), Quaternion.identity);
+    IEnumerator Draw()
+    {
+        float x = 90.0f; //Card is face down when instantiated
+        int index = Random.Range(0, cards.Length);//Select a random card
+        value = index % 13 + 1;//Get numerical value of card
+        GameObject dealt = Instantiate(cards[index], new Vector3(0.0f, 22.0f, 0.0f), Quaternion.Euler(90.0f, 0.0f, 0.0f));//Deal a card
+
+        //Rotate the card on its x-axis to reveal number
+        while (dealt.transform.rotation.x > 0.0f)
+        {
+            x -= 2.0f;
+            dealt.transform.rotation = Quaternion.Euler(x, 0.0f, 0.0f);
+            yield return new WaitForSeconds(0.00000000000000000001f);
+        }
+        dealt.transform.rotation = Quaternion.Euler(0.0f, 0.0f, 0.0f);
+    }
+
+    //Return the value of the most recently dealt card
+    public int getValue() {
+        return value;
     }
 
     //Change the scale of all cards so that it may be easily seen be all players
-    public void ScaleCards() {
+    public void ScaleCards()
+    {
         if (cards.Length > 0)
         {
-            Vector3 scale = new Vector3(50f, 50f, 50f); //Standard scale for now
-            Vector3 two = new Vector3(2f, 2f, 2f);
-            for (int i = 1; i < cards.Length; i++)
+            Vector3 scale = new Vector3(50.0f, 50.0f, 50.0f); //Standard scale for now
+            for (int i = 0; i < cards.Length; i++)
             {
                 cards[i].gameObject.transform.localScale = scale;
+                cards[i].gameObject.tag = "Card";
             }
-            cards[0].gameObject.transform.localScale = two;
         }
     }
 }
