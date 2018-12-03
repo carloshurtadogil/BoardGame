8a2a998e43c109cb0033cc298c5f61c1478150cd
ficsby <30848781+ficsby@users.noreply.github.com>
2018-12-03 10:53:17 -0800

    Add files via upload
    Minigame pt 1

---
+++ Super Software Design and Architecture Party/Assets/FPS_Minigame/FPS Scripts/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace S3
+{
+    public class Bullet : MonoBehaviour
+    {
+        void OnCollisionEnter(Collision collision)
+        {
+            var hit = collision.gameObject;
+            var health = hit.GetComponent<Health>();
+
+            if (health != null)
+            {
+                health.TakeDamage(10);
+            }
+
+            Destroy(gameObject);
+        }
+    }
+}
+
---
+++ Super Software Design and Architecture Party/Assets/FPS_Minigame/FPS Scripts/Health.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class Health : NetworkBehaviour
+{
+    [SerializeField] private Text lifeText;
+
+    public const int maxHealth = 100;
+    public const int maxLives = 20;
+
+    [SyncVar(hook = "OnChangeHealth")]
+    public int currentHealth = maxHealth;
+    [SyncVar(hook = "LoseLife")]
+    private int lives;
+
+    public RectTransform healthBar;
+
+    void Start()
+    {
+        lives = maxLives;
+        lifeText.text = lives.ToString();
+    }
+
+    public void TakeDamage(int amount)
+    {
+        if (!isServer)
+            return;
+
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = maxHealth;
+            // called on the Server, but invoked on the Clients
+            RpcRespawn();
+        }
+    }
+
+    void OnChangeHealth(int currentHealth)
+    {
+        healthBar.sizeDelta = new Vector2(currentHealth, healthBar.sizeDelta.y);
+    }
+
+    void LoseLife(int lives)
+    {
+        lifeText.text = lives.ToString();
+    }
+
+    [ClientRpc]
+    void RpcRespawn()
+    {
+        lives -= 1;
+        LoseLife(lives);
+        if (isLocalPlayer)
+        {
+            // move back to zero location
+            transform.position = Vector3.zero;
+        }
+    }
+}
