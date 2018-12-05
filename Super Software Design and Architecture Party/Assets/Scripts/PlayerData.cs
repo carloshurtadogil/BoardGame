537b2d5fbbee3c9bad44e59d6e8af6c6ea7bc30a
Carlos Hurtado <carlos.hurtado19@yahoo.com>
2018-12-05 14:39:57 -0800

    File Changes

--- Super Software Design and Architecture Party/Assets/Scripts/PlayerData.cs
+++ Super Software Design and Architecture Party/Assets/Scripts/PlayerData.cs
@@ -11,7 +11,10 @@ public class PlayerData : NetworkBehaviour {
     public Material[] headSkins;
     public GameObject[] spawnPoints;
 
+    private bool startScanning;
+    private int newScan;
     private int playerId;
+    private int playersInRoom;
 
     void Start()
     {
@@ -21,6 +24,21 @@ public class PlayerData : NetworkBehaviour {
             Camera.main.transform.rotation = q;//LookAt(this.transform.position*20);
             Camera.main.transform.parent = this.transform;
             //StartCoroutine("CharacterUpdate");
+        }
+    }
+
+    void Update()
+    {
+        if(!isLocalPlayer) {
+            return;
+        }
+
+        if(startScanning) {
+            newScan = ScanForPlayers();
+            if(newScan != playersInRoom) {
+                playersInRoom = newScan;
+                ReskinOthersInLocal();
+            }
         }
     }
 
@@ -43,6 +61,7 @@ public class PlayerData : NetworkBehaviour {
         {
             Debug.Log("This should appear");
             playerId = ScanForPlayers();
+            playersInRoom = playerId;
             Debug.Log("PlayerId = " + playerId);
             switch (playerId)
             {
@@ -50,7 +69,7 @@ public class PlayerData : NetworkBehaviour {
                     RpcSpawn(0, 0);
                     break;
                 case 2:
-                    RpcSpawn(2, 1);
+                    RpcSpawn(3, 1);
                     break;
                 case 3:
                     RpcSpawn(2, 2);
@@ -69,18 +88,55 @@ public class PlayerData : NetworkBehaviour {
         }
     }
 
+    public int GetPlayerID() {
+        return playerId;
+    }
+
     //[ClientRpc]
     public void RpcSpawn(int c, int pos)
     {
-        if(isLocalPlayer) {
-            Debug.Log("RpcSpawn param is " + c);
-            SkinnedMeshRenderer r = gameObject.transform.GetComponentInChildren<SkinnedMeshRenderer>();
-            Material[] mats = { bodySkins[c], headSkins[c] };
-            r.materials = mats;
-            Vector3 spawn = spawnPoints[pos].transform.position;
+        Debug.Log("RpcSpawn param is " + c);
+        SkinnedMeshRenderer r = gameObject.transform.GetComponentInChildren<SkinnedMeshRenderer>();
+        Material[] mats = { bodySkins[c], headSkins[c] };
+        r.materials = mats;
+        Vector3 spawn = spawnPoints[pos].transform.position;
 
-            gameObject.transform.position = new Vector3(spawn.x, gameObject.transform.position.y, spawn.z);
-            Debug.Log("Spawn position is supposed to be " + spawn + "\nActual position is " + gameObject.transform.position);
+        gameObject.transform.position = new Vector3(spawn.x, gameObject.transform.position.y, spawn.z);
+        Debug.Log("Spawn position is supposed to be " + spawn + "\nActual position is " + gameObject.transform.position);
+        startScanning = true;
+    }
+
+    public void ReskinOthersInLocal() {
+        GameObject[] playersInRoomObjects = GameObject.FindGameObjectsWithTag("Players");
+        SortPlayers(playersInRoomObjects);
+        for (int i = 0; i < playersInRoomObjects.Length; i++) { 
+            if(playersInRoomObjects[i].GetComponent<PlayerData>().playerId != this.playerId) {
+                Material[] m = { bodySkins[i], headSkins[i] };
+                playersInRoomObjects[i].gameObject.transform.GetComponentInChildren<SkinnedMeshRenderer>().materials = m;
+            }
+        }
+    }
+
+    public void SortPlayers(GameObject[] p) {
+        int sid, smallest, current;
+        GameObject temp;
+        Debug.Log("Length of list: " + p.Length);
+        for(int i = 0; i < p.Length; i++) {
+            sid = i; 
+            smallest = p[i].GetComponent<PlayerData>().GetPlayerID();
+            for (int j = i; j < p.Length; j++) {
+                current = p[j].GetComponent<PlayerData>().GetPlayerID();
+                if ( current < smallest ) {
+                    smallest = current;
+                    sid = j;
+                }
+            }
+            temp = p[sid];
+            p[sid] = p[i];
+            p[i] = temp;
+        }
+        foreach(GameObject pl in p) {
+            Debug.Log("PID: " + pl.GetComponent<PlayerData>().playerId);
         }
     }
 }
